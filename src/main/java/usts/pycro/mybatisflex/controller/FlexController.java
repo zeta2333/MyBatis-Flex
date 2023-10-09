2a81b6f8bc99808f7165db08b38ccc0398ebd9ee
Pycro <pycro6815@163.com>
2023-10-09 14:06:35 +0800

    10.9 update

--- src/main/java/usts/pycro/mybatisflex/controller/FlexController.java
+++ src/main/java/usts/pycro/mybatisflex/controller/FlexController.java
@@ -1,6 +1,7 @@
 package usts.pycro.mybatisflex.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import usts.pycro.mybatisflex.entity.Account;
@@ -8,6 +9,8 @@ import usts.pycro.mybatisflex.service.AccountService;
 
 import javax.annotation.Resource;
 import java.util.List;
+
+import static usts.pycro.mybatisflex.entity.table.AccountTableDef.ACCOUNT;
 
 /**
  * @author Pycro
@@ -23,5 +26,15 @@ public class FlexController {
     @GetMapping("listAccounts")
     public List<Account> getAllAccounts() {
         return accountService.list();
+    }
+
+    @GetMapping("listByQuery/{id}")
+    public List<Account> getByQuery(@PathVariable("id") Long id) {
+        List<Account> list = accountService.queryChain()
+                .select(ACCOUNT.DEFAULT_COLUMNS)
+                .from(ACCOUNT)
+                .where(ACCOUNT.ID.ge(id))
+                .list();
+        return list;
     }
 }
